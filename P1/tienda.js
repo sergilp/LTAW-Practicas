4ce4c1b2ef1e782cf425949645a891391c74ba2b
sergilp <s.gilp.2020@alumnos.urjc.es>
2025-04-15 12:50:10 +0200

    Trabajando en P1 - tienda

---
+++ P1/tienda.js
@@ -0,0 +1,10 @@
+//-- Importar módulos
+
+const http = require('http');   //-- Módulo para crear servidores HTTP
+const fs = require('fs');       //-- Módulo para leer archivos
+const path = require('path');   //-- Módulo para rutas de archivos
+
+//-- Puerto a utilizar 
+const PORT = 8090;
+
+const pagina_error = fs.readFileSync('./Pages/pagina_error.html','utf8');
