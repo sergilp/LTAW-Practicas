e9a2b65203eb3d8d6114c2e635b675a37f9c22c2
sergilp <s.gilp.2020@alumnos.urjc.es>
2025-04-22 12:53:30 +0200

    Avanzando

--- P1/tienda.js
+++ P1/tienda.js
@@ -7,3 +7,28 @@ const path = require('path');   //-- Módulo para rutas de archivos
 const PORT = 8090;
 
 const pagina_error = fs.readFileSync('./Pages/pagina_error.html','utf8');
+
+
+//-- Función para leer archivos
+function leerFichero(fichero, callback) {
+    fs.readFile(fichero, (err, data) => {
+        if (err) {
+            console.error('No se puede leer el archivo:', fichero, err);
+            callback(err, null);
+        } else {
+            console.log(`Lectura correcta de ${fichero}`);
+            callback(null, data);
+                }
+    });
+}
+
+const server = http.createServer((req, res) => {
+    console.log('Petición recibida:', req.url);
+
+    //-- Se declara el Content-Type y recurso
+    if (req.url.endsWith('png')) {          //-- Acceso a archivos PNG
+            content_type = 'image/png';
+            recurso = './Images/' + req.url.split('/').pop();
+            
+    }
+})
